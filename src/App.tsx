03db1b433cb268bc60599c7ec6ee5132904e1792
TomBombadilom <tom@croll.fr>
2024-03-05 13:56:14 +0100

    try to set fullscreen on load for PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import './App.css';
-import { ThemeProvider } from './lib/ThemeProvider.tsx';
+import { useTheme, ThemeProvider } from './lib/ThemeProvider.tsx';
 import loading from './lib/loading';
 import { ErrorBoundary } from './lib/ErrorBoundary';
 
-import {Background} from './lib/background/Background.tsx';
-// Lazy load the Routes component to split code and only load it when needed
+import { Background } from './lib/background/Background.tsx';
 const Routes = lazy(() => import('./Routes'));
 
-// Define the App component with ErrorBoundary and Suspense for handling loading and errors
 const App = () => {
+  const { theme } = useTheme();
+  const isDarkMode = theme === 'dark';
+
+  // Function to enter full screen
+  const enterFullScreen = () => {
+    const elem = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen().catch((e) => {
+        console.error(`Error attempting to enable full-screen mode: ${e.message} (${e.name})`);
+      });
+    }
+  };
+
+  useEffect(() => {
+    // Automatically enter full screen on component mount
+    enterFullScreen();
+
+    // Clean up
+    return () => {
+      // Exit full screen when component unmounts
+      if(document.fullscreenElement) {
+        document.exitFullscreen();
+      }
+    };
+  }, []);
+
   return (
     <ErrorBoundary>
-      <Background speed={.25} />
+      <Background speed={0.25} isDarkMode={isDarkMode}/>
       <Suspense fallback={loading()}>
         <ThemeProvider defaultTheme={"dark"} storageKey={"vite-ui-theme"}>
           <Routes />
         </ThemeProvider>
       </Suspense>
     </ErrorBoundary>
-  )
+  );
 };
 
 export default App;
