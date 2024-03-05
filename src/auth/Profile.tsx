e8de5bd66c181ddef614ddebdab2dfe5ab62ca5d
TomBombadilom <tom@croll.fr>
2024-03-05 09:47:43 +0100

    add adaptative behaviour on profile

--- src/auth/Profile.tsx
+++ src/auth/Profile.tsx
@@ -50,89 +50,89 @@ const Profile = () => {
   }
   return (
     <Card className=" w-[80vw] overflow-x-hidden border-0 p-5 rounded-xl backdrop-filter backdrop-blur-lg bg-card bg-opacity-medium text-card-foreground dark:bg-card dark:bg-opacity-medium dark:text-primary-foreground h-fit">
-      <CardTitle className="flex flex-col content-start items-center gap-2">
+      <CardTitle className="flex flex-col gap-2 content-start items-center">
         <Avatar className="w-12 h-12">
           <AvatarImage src="../icon/favicon-32x32.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
         Helia IPFS CMS
       </CardTitle>
-      <div className="grid grid-cols-2 content-start">
+      <div className="grid grid-cols-1 content-start md:grid-cols-2">
         <CardHeader className="flex w-[20vw] items-center">
           <CardDescription >
             <Avatar className="w-32 h-32">
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <h1 className="text-center text-xl font-bold">John Doe</h1>
+            <h1 className="text-xl font-bold text-center">John Doe</h1>
         </CardDescription>
         </CardHeader>
         <CardContent className="w-[60vw]">
-          <div className="min-h-fit pt-2 font-mono my-16">
+          <div className="pt-2 my-16 font-mono min-h-fit">
             <div className="container mx-auto">
-              <div className="Input  max-w-2xl p-6 mx-auto">
+              <div className="p-6 mx-auto max-w-2xl Input">
                 <h2 className="text-2xl text-gray-900">Account Setting</h2>
-                <form className="mt-6 border-t border-gray-400 pt-4">
+                <form className="pt-4 mt-6 border-t border-gray-400">
                   <div className='flex flex-wrap -mx-3 mb-6'>
-                    <div className='w-full md:w-full px-3 mb-6'>
-                      <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' htmlFor='grid-text-1'>email address</label>
-                      <Input className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' id='grid-text-1' type='text' placeholder='Enter email' required />
+                    <div className='px-3 mb-6 w-full md:w-full'>
+                      <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase' htmlFor='grid-text-1'>email address</label>
+                      <Input className='block px-4 py-3 w-full leading-tight text-gray-700 bg-white rounded-md border border-gray-400 shadow-inner appearance-none focus:outline-none focus:border-gray-500' id='grid-text-1' type='text' placeholder='Enter email' required />
                     </div>
-                    <div className='w-full md:w-full px-3 mb-6 '>
-                      <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>password</label>
-                      <button className="appearance-none bg-gray-200 text-gray-900 px-2 py-1 shadow-sm border border-gray-400 rounded-md ">change your password</button>
+                    <div className='px-3 mb-6 w-full md:w-full'>
+                      <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'>password</label>
+                      <button className="px-2 py-1 text-gray-900 bg-gray-200 rounded-md border border-gray-400 shadow-sm appearance-none">change your password</button>
                     </div>
-                    <div className='w-full md:w-full px-3 mb-6'>
-                      <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>pick your country</label>
-                      <div className="flex-shrink w-full inline-block relative">
-                        <select className="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
+                    <div className='px-3 mb-6 w-full md:w-full'>
+                      <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'>pick your country</label>
+                      <div className="inline-block relative flex-shrink w-full">
+                        <select className="block px-4 py-2 pr-8 w-full text-gray-600 bg-white rounded border border-gray-400 shadow-inner appearance-none">
                           <option>choose ...</option>
                           <option>USA</option>
                           <option>France</option>
                           <option>Spain</option>
                           <option>UK</option>
                         </select>
-                        <div className="pointer-events-none absolute top-0 mt-3  right-0 flex items-center px-2 text-gray-600">
-                          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                        <div className="flex absolute top-0 right-0 items-center px-2 mt-3 text-gray-600 pointer-events-none">
+                          <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                         </div>
                       </div>
                     </div>
-                    <div className='w-full md:w-full px-3 mb-6'>
-                      <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>fav language</label>
-                      <div className="flex-shrink w-full inline-block relative">
-                        <select className="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
+                    <div className='px-3 mb-6 w-full md:w-full'>
+                      <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'>fav language</label>
+                      <div className="inline-block relative flex-shrink w-full">
+                        <select className="block px-4 py-2 pr-8 w-full text-gray-600 bg-white rounded border border-gray-400 shadow-inner appearance-none">
                           <option>choose ...</option>
                           <option>English</option>
                           <option>France</option>
                           <option>Spanish</option>
                         </select>
-                        <div className="pointer-events-none absolute top-0 mt-3  right-0 flex items-center px-2 text-gray-600">
-                          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                        <div className="flex absolute top-0 right-0 items-center px-2 mt-3 text-gray-600 pointer-events-none">
+                          <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                         </div>
                       </div>
                     </div>
-                    <div className="personal w-full border-t border-gray-400 pt-4">
+                    <div className="pt-4 w-full border-t border-gray-400 personal">
                       <h2 className="text-2xl text-gray-900">Personal info:</h2>
-                      <div className="flex items-center justify-between mt-4">
-                        <div className='w-full md:w-1/2 px-3 mb-6'>
-                          <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >first name</label>
-                          <Input className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text' required />
+                      <div className="flex justify-between items-center mt-4">
+                        <div className='px-3 mb-6 w-full md:w-1/2'>
+                          <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase' >first name</label>
+                          <Input className='block px-4 py-3 w-full leading-tight text-gray-700 bg-white rounded-md border border-gray-400 shadow-inner appearance-none focus:outline-none focus:border-gray-500' type='text' required />
                         </div>
-                        <div className='w-full md:w-1/2 px-3 mb-6'>
-                          <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >last name</label>
-                          <Input className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text' required />
+                        <div className='px-3 mb-6 w-full md:w-1/2'>
+                          <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase' >last name</label>
+                          <Input className='block px-4 py-3 w-full leading-tight text-gray-700 bg-white rounded-md border border-gray-400 shadow-inner appearance-none focus:outline-none focus:border-gray-500' type='text' required />
                         </div>
                       </div>
-                      <div className='w-full md:w-full px-3 mb-6'>
-                        <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>user name</label>
-                        <Input className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text' required />
+                      <div className='px-3 mb-6 w-full md:w-full'>
+                        <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase'>user name</label>
+                        <Input className='block px-4 py-3 w-full leading-tight text-gray-700 bg-white rounded-md border border-gray-400 shadow-inner appearance-none focus:outline-none focus:border-gray-500' type='text' required />
                       </div>
-                      <div className='w-full md:w-full px-3 mb-6'>
-                        <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Bio</label>
-                        <textarea className='bg-gray-100 rounded-md border leading-normal resize-none w-full h-20 py-2 px-3 shadow-inner border border-gray-400 font-medium placeholder-gray-700 focus:outline-none focus:bg-white' required></textarea>
+                      <div className='px-3 mb-6 w-full md:w-full'>
+                        <label className='block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase' >Bio</label>
+                        <textarea className='px-3 py-2 w-full h-20 font-medium leading-normal placeholder-gray-700 bg-gray-100 rounded-md border border-gray-400 shadow-inner resize-none focus:outline-none focus:bg-white' required></textarea>
                       </div>
                       <div className="flex justify-end">
-                        <button className="appearance-none bg-gray-200 text-gray-900 px-2 py-1 shadow-sm border border-gray-400 rounded-md mr-3" type="submit">save changes</button>
+                        <button className="px-2 py-1 mr-3 text-gray-900 bg-gray-200 rounded-md border border-gray-400 shadow-sm appearance-none" type="submit">save changes</button>
                       </div>
                     </div>
                   </div>
@@ -150,7 +150,7 @@ const Profile = () => {
                     <FormLabel>Email</FormLabel>
                     <FormControl>
                       <Input
-                        className="Input Input-bordered w-full"
+                        className="w-full Input Input-bordered"
                         type="email"
                         {...field}
                       />
@@ -167,22 +167,22 @@ const Profile = () => {
         </CardContent>
       </div>
       <CardFooter
-        className="flex justify-between w-full flex-row"
+        className="flex flex-row justify-between w-full"
       >
-        <div className="btn-wrapper w-1/2">
+        <div className="w-1/2 btn-wrapper">
           <Button
             type='submit'
             onClick={() => onSubmit(form.getValues())}
-            className="w-full m-auto bg-card text-card-foreground border-0 pr-3">
+            className="pr-3 m-auto w-full border-0 bg-card text-card-foreground">
             Save
           </Button>
-          <div className="btn-bg w-1/2"></div>
+          <div className="w-1/2 btn-bg"></div>
         </div>
-        <div className="btn-wrapper w-1/2">
-          <Button className="w-full m-auto bg-card text-card-foreground border-0 pr-3">
+        <div className="w-1/2 btn-wrapper">
+          <Button className="pr-3 m-auto w-full border-0 bg-card text-card-foreground">
             Forgot your login ?
           </Button>
-          <div className="btn-bg w-1/2"></div>
+          <div className="w-1/2 btn-bg"></div>
         </div>
 
       </CardFooter>
